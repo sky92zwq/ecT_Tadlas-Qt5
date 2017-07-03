6aa1830388a0774d2527aff262e9be278e84ee21
zwq <zhangwenqiangwy@163.com>
2017-07-03 19:00:55 +0800

    matlabdynamic

---
+++ matlabdynamic/calderon_circle_16_electrodes_ssjwq_types.h
@@ -0,0 +1,19 @@
+/*
+ * File: calderon_circle_16_electrodes_ssjwq_types.h
+ *
+ * MATLAB Coder version            : 3.0
+ * C/C++ source code generated on  : 15-Nov-2016 16:51:56
+ */
+
+#ifndef __CALDERON_CIRCLE_16_ELECTRODES_SSJWQ_TYPES_H__
+#define __CALDERON_CIRCLE_16_ELECTRODES_SSJWQ_TYPES_H__
+
+/* Include Files */
+#include "rtwtypes.h"
+#endif
+
+/*
+ * File trailer for calderon_circle_16_electrodes_ssjwq_types.h
+ *
+ * [EOF]
+ */
